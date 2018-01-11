ba4276d8c9d926cb45d256c95392704540f1caf0
Street <cstreet@deloitte.com>
2018-01-11 15:22:04 -0500

    change to comicvine api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,20 +26,16 @@ angular.module('marvelApp')
           // you need a new ts every request                                                                                    
           var ts = new Date().getTime();
           var hash = CryptoJS.MD5(ts + $scope.PRIV_KEY + $scope.PUBLIC_KEY).toString();                                                                   
-          var url = 'https://gateway.marvel.com/v1/public/characters';
+          var url = 'https://api-comic-vine.herokuapp.com/characters';
           $.getJSON(url, {
-              nameStartsWith: searchString,
-              ts: ts,
-              apikey: $scope.PUBLIC_KEY,
-              hash: hash
+              type: "GET",
+              filter: "name:" + searchString,
+              api_key: $scope.key,
+              format: "json"
           })
           .done(function (data) {
-              if(data.data.results[0].length === 1){
-                var background = "url(" +  data.data.results[0].thumbnail.path + "." + data.data.results[0].thumbnail.extension + ")";
-                $('.bg').css('background-image', background);
-                window.location.replace('/#!/about');
-              } else if(data.data.results.length > 1){
-                $scope.searchResults = data.data.results;
+              if(data.results.length > 1){
+                $scope.searchResults = data.results;
               } else {
                 console.log("No Results");
               }
@@ -49,29 +45,15 @@ angular.module('marvelApp')
           .fail(function (err) {
               console.log(err);
           });
-
-          //TRYING COMIC VINE API
-          $.ajax("https://comicvine.gamespot.com/api/characters", {
-              type: "GET",
-              filter: "name:" + searchString,
-              api_key: $scope.key,
-              format: "json"
-          })
-          .done(function (data) {
-              console.log(data);
-          })
-          .fail(function (err) {
-              console.log(err);
-          });
       };
 
       function selectHero(result){
-        console.log(result);
         $scope.selectedResult = result;
-        var background = "url(" +  result.thumbnail.path + "." + result.thumbnail.extension + ")";
+        var background = "url(" +  result.image.super_url + ")";
         $('.hero').css('background-image', background);
         
-        // window.location.replace('/#!/about');
+        $("#current-event").empty();
+        $("#current-event").append(result.description);
       }
 
       function init() {
